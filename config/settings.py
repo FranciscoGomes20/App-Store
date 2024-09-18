9d37e9f8c037a68415313e70d3bf756220d46e50
FranciscoGomes20 <henriquecgomes10@gmail.com>
2024-09-18 14:07:11 -0500

    API URL has been inserted into ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('DJANGO_HOSTS')]
 
 
 # Application definition
