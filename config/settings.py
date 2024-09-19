9d2facc9fd2999404a4a3eda1952fc6a1440a740
FranciscoGomes20 <henriquecgomes10@gmail.com>
2024-09-19 10:57:11 -0500

    Updated the ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [os.getenv('DJANGO_HOSTS')]
+#ALLOWED_HOSTS = [os.getenv('DJANGO_HOSTS')]
+ALLOWED_HOSTS = [
+    os.getenv('DJANGO_HOSTS'),
+    'localhost',
+    '127.0.0.1',
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'https://app-store-production-840d.up.railway.app',
