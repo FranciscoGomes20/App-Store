9ef8ca3826718c206c560984a8665b1893f42bd8
FranciscoGomes20 <henriquecgomes10@gmail.com>
2024-09-20 10:58:17 -0500

    Takes the data from the DB and brings it to the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Aguarda o banco de dados estar disponível
-/wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Database is up!"
+# Extrai o host e a porta a partir da variável DATABASE_URL
+DB_HOST=$(echo $DATABASE_URL | awk -F[/:] '{print $4}')
+DB_PORT=$(echo $DATABASE_URL | awk -F[/:] '{print $5}')
+
+# Espera o banco de dados ficar disponível
+/wait-for-it.sh $DB_HOST:$DB_PORT --timeout=30 --strict -- echo "Database is up!"
 
 # Executa as migrações
 python manage.py migrate
